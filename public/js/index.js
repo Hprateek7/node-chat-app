694aa7f185aede53ff9120d488b8abd91241484a
Harsh Prateek <hprateek1@gmail.com>
2018-09-18 13:54:28 +0530

    add mustache for templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,11 +9,21 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a')
-    var li= jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    jQuery('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a')
+    var tempalate= jQuery('#message-tempalate').html();
+    var html = Mustache.render(tempalate,{
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+
+    // var formattedTime = moment(message.createdAt).format('h:mm a')
+    // var li= jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // jQuery('#messages').append(li);
 });
 
 // socket.emit('createMessage',  {
@@ -57,13 +67,22 @@ locationButton.on('click', function (){
     });
 
     socket.on('newLocationMessage', function (message) {
-        var formattedTime = moment(message.createdAt).format('h:mm a')
-        var li = jQuery('<li></li>');
-        var a = jQuery('<a target="_blank">My current Location</a>');
-        li.text(`${message.from} ${formattedTime}: `);
-        a.attr('href', message.url);
-        li.append(a);
-        jQuery('#messages').append(li);
+        var formattedTime = moment(message.createdAt).format('h:mm a');
+        var tempalate = jQuery('#location-message-tempalate').html();
+        var html = Mustache.render(tempalate, {
+            from: message.from,
+            url: message.url,
+            createdAt: formattedTime
+        });
+        jQuery('#messages').append(html);
+
+        // var formattedTime = moment(message.createdAt).format('h:mm a')
+        // var li = jQuery('<li></li>');
+        // var a = jQuery('<a target="_blank">My current Location</a>');
+        // li.text(`${message.from} ${formattedTime}: `);
+        // a.attr('href', message.url);
+        // li.append(a);
+        // jQuery('#messages').append(li);
     });
 
 
